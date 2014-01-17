774cc7e75b613eb32847f5512d626ae484274749
Janusz Kowalczyk <kowalczykjanusz@gmail.com>
2014-01-17 14:20:05 +0000

    another one

--- fabfile.py
+++ fabfile.py
@@ -137,8 +137,8 @@ def terminate_ec2_instances():
 
 def start_flaskrilio():
     global DIST
-    #with cd('/tmp/flaskrilio/%s/flaskrilio' % DIST):
-    sudo('sh -c \'cd /tmp/flaskrilio/%s/flaskrilio\' && MODE=EC2 nohup /tmp/flaskrilio/%s/flaskrilio/flaskriliosrv.py &> /tmp/flaskrilio/%s/flaskrilio/reports/nohup.txt &' % (DIST, DIST, DIST), pty=False)
+    with cd('/tmp/flaskrilio/%s/flaskrilio' % DIST):
+        sudo('MODE=EC2 nohup flaskriliosrv.py &> reports/nohup.txt &')
 
 
 def stop_flaskrilio():
